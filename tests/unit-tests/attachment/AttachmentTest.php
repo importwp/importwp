104b6698b5a922b13fed81be53c2648c09f0b01b
James Collings <james@jclabs.co.uk>
2017-03-31 23:09:45 +0100

    Add basic attachment unit test

---
+++ tests/unit-tests/attachment/AttachmentTest.php
@@ -0,0 +1,29 @@
+<?php
+
+class AttachmentTest extends WP_UnitTestCase {
+
+	var $importer;
+
+	public function setUp() {
+		parent::setUp();
+		$this->importer = $GLOBALS['jcimporter'];
+	}
+
+	public function test_wp_insert_attachment(){
+
+		$parent_id = wp_insert_post( array( 'post_title'  => 'INSERTED',
+		                                    'post_type'   => 'post',
+		                                    'post_status' => 'publish'
+		) );
+
+		$attachment = new JCI_Attachment();
+		$post_id = $attachment->wp_insert_attachment($parent_id, $this->importer->get_plugin_dir() . '/tests/data/data-pages.xml');
+
+		$post = get_post($post_id);
+		$this->assertEquals(1, preg_match('/\/data-pages\.xml$/', $post->guid));
+		$this->assertEquals('attachment', $post->post_type);
+		$this->assertEquals($parent_id, $post->post_parent);
+		$this->assertEquals('inherit', $post->post_status);
+	}
+
+}
