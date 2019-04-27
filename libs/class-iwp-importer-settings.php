977cac9eee74d0d24178a8473744e1967af30a68
James Collings <james@jclabs.co.uk>
2019-04-27 12:59:33 +0100

    Remove int from function variable

--- libs/class-iwp-importer-settings.php
+++ libs/class-iwp-importer-settings.php
@@ -585,9 +585,9 @@ class IWP_Importer_Settings {
 	 *
 	 * This will cause the importer to re-process the file.
 	 *
-	 * @param int $id
+	 * @param integer $id
 	 */
-	public static function clear_edit_config( int $id ) {
+	public static function clear_edit_config( $id ) {
 
 		$config_file = JCI()->get_tmp_config_path($id);
 		if(file_exists($config_file)){
