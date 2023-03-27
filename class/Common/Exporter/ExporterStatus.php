fc3254866b383214cbb92f2e1d2e43aadcf37f2e
removedporn <86824510+removedporn@users.noreply.github.com>
2023-03-27 12:13:01 +0800

    Delete class directory

--- class/Common/Exporter/ExporterStatus.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace ImportWP\Common\Exporter;
-
-class ExporterStatus
-{
-}
--- class/Common/Importer/PermissionInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace ImportWP\Common\Importer;
-
-interface PermissionInterface
-{
-    public function validate($fields, $method, $group_id);
-    public function allowed_method($method);
-}
