b1b6afd6959c03d6d1edb15708b92f7b198ce6b7
James Collings <james@jclabs.co.uk>
2023-11-09 22:55:40 +0000

    Build v2.8.3

--- dist/index.asset.php
+++ dist/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-hooks'), 'version' => '9a7c24e86e01f6081b44');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-hooks'), 'version' => '7c304dc9fd9b1cb11ac6');
