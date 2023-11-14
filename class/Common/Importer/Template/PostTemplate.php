c02bbaa93b714e12af580cf46f490b13274c0b24
James Collings <james@jclabs.co.uk>
2023-11-14 22:47:12 +0000

    update post status tooltip

--- class/Common/Importer/Template/PostTemplate.php
+++ class/Common/Importer/Template/PostTemplate.php
@@ -120,7 +120,7 @@ class PostTemplate extends Template implements TemplateInterface
                     ['value' => 'private', 'label' => 'Private'],
                     ['value' => 'trash', 'label' => 'Trash']
                 ],
-                'tooltip' => __('Whether the post can accept comments. Accepts open or closed', 'importwp')
+                'tooltip' => __('The status of a given post determines how WordPress handles that post', 'importwp')
             ]),
             $this->register_group('Parent Settings', '_parent', [
                 $this->register_field('Parent', 'parent', [
